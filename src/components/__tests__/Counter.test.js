de95a301f532b456d192d477da6e4ae1b06856cc
CarnunMP <carnun@hotmail.co.uk>
2019-10-03 17:29:57 +0100

    upper/lower limit tests done

--- src/components/__tests__/Counter.test.js
+++ src/components/__tests__/Counter.test.js
@@ -7,7 +7,7 @@ let tools;
 
 beforeEach(() => {
   rtl.cleanup();
-  tools = rtl.render(<Counter user='Peter' />);
+  tools = rtl.render(<Counter user='Peter' countLimit={5} />);
 });
 
 describe('Counter component', () => {
@@ -60,11 +60,23 @@ describe('Counter component', () => {
   });
 
   it('prevents the count from going over an upper limit', () => {
-    // implement
+    const incButton = tools.queryByTestId('incButton');
+    
+    for (let i = 0; i < 10; i++) {
+      rtl.fireEvent.click(incButton);
+    }
+
+    expect(tools.queryByText(/5/)).toBeInTheDocument(); // I set this upper limit above.
   });
 
   it('prevents the count from going under a lower limit', () => {
-    // implement
+    const decButton = tools.queryByTestId('decButton');
+
+    for (let i = 0; i < 10; i++) {
+      rtl.fireEvent.click(decButton);
+    }
+
+    expect(tools.queryByText(/-5/)).toBeInTheDocument();
   });
 
   it('shows a warning once we hit the upper limit of the counter', () => {
